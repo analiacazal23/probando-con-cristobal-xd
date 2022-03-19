db30947a3842f7940e5daebfb4ce7fe19105e9b3
lp1-2022 <analiacazal@gmail.com>
2022-03-19 10:37:37 -0300

    ke aze wey :v

---
+++ hola/src/hola.c
@@ -0,0 +1,18 @@
+/*
+ ============================================================================
+ Name        : hola.c
+ Author      : Analia Cazal
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("hola");
+	return EXIT_SUCCESS;
+
+}
